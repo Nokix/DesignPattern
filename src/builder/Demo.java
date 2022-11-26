a2578db37f22b28a0ebb4edf426c3e98465e3db9
Viktor Reichert <viktor.reichert@conteco.gmbh>
2022-11-26 16:22:19 +0100

    Builer: Car+Carmanual

---
+++ src/builder/Demo.java
@@ -0,0 +1,32 @@
+package builder;
+
+import builder.builders.CarBuilder;
+import builder.builders.CarManualBuilder;
+import builder.cars.Car;
+import builder.cars.Manual;
+import builder.director.Director;
+
+/**
+ * EN: Demo class. Everything comes together here.
+ *
+ * RU: Демо-класс. Здесь всё сводится воедино.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        Director director = new Director();
+        CarBuilder builder = new CarBuilder();
+        director.constructSportsCar(builder);
+
+        Car car = builder.getResult();
+        System.out.println("Car built:\n" + car.getCarType());
+
+
+        CarManualBuilder manualBuilder = new CarManualBuilder();
+
+        director.constructSportsCar(manualBuilder);
+        Manual carManual = manualBuilder.getResult();
+        System.out.println("\nCar manual built:\n" + carManual.print());
+    }
+
+}
